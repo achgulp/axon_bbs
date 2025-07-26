0d039a88aea07c0b756137863dab6c031b49505d
achgulp <achgulp@jamdata.net>
2025-07-26 18:54:54 -0400

    Update 20250726185454

--- core/admin.py
+++ core/admin.py
@@ -10,6 +10,7 @@ import base64
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from .services.encryption_utils import generate_checksum
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -48,16 +49,20 @@ class ContentExtensionRequestAdmin(admin.ModelAdmin):
 
 @admin.register(TrustedInstance)
 class TrustedInstanceAdmin(admin.ModelAdmin):
-    list_display = ('pubkey', 'web_ui_onion_url', 'p2p_onion_address', 'added_at')
-    actions = ['generate_keys', 'generate_test_script']
+    list_display = ('web_ui_onion_url', 'p2p_onion_address', 'pubkey_checksum', 'added_at')
+    actions = ['generate_keys']
+    readonly_fields = ('pubkey_checksum',)
+
+    @admin.display(description='Pubkey Checksum')
+    def pubkey_checksum(self, obj):
+        return generate_checksum(obj.pubkey)
 
     def generate_keys(self, request, queryset):
         key = base64.urlsafe_b64encode(settings.SECRET_KEY.encode()[:32])
         f = Fernet(key)
-
         for instance in queryset:
             private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-            public_key = private_key.public_key().public_bytes(
+            public_key_pem = private_key.public_key().public_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo
             ).decode('utf-8')
@@ -66,54 +71,12 @@ class TrustedInstanceAdmin(admin.ModelAdmin):
                 format=serialization.PrivateFormat.PKCS8,
                 encryption_algorithm=serialization.NoEncryption()
             ).decode('utf-8')
-
             encrypted_private = f.encrypt(private_pem.encode()).decode()
-
-            instance.pubkey = public_key
+            instance.pubkey = public_key_pem
             instance.encrypted_private_key = encrypted_private
             instance.save()
-
         self.message_user(request, "Keys generated and encrypted for selected instances.")
-
-    generate_keys.short_description = "Generate and encrypt keys"
-
-    def generate_test_script(self, request, queryset):
-        if queryset.count() != 1:
-            self.message_user(request, "Select exactly one instance to generate test script.", level='error')
-            return
-
-        instance = queryset.first()
-        test_magnet = "magnet:?xt=urn:btih:testkeyverification&dn=keytest"
-
-        key = base64.urlsafe_b64encode(settings.SECRET_KEY.encode()[:32])
-        f = Fernet(key)
-        if instance.encrypted_private_key:
-            private_pem = f.decrypt(instance.encrypted_private_key.encode()).decode()
-            private_key = serialization.load_pem_private_key(private_pem.encode(), password=None)
-            hash_ctx = hashes.Hash(hashes.SHA256())
-            hash_ctx.update(test_magnet.encode())
-            digest = hash_ctx.finalize()
-            signature = private_key.sign(
-                digest,
-                padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
-                hashes.SHA256()
-            )
-            signature_b64 = base64.b64encode(signature).decode()
-        else:
-            signature_b64 = "NO_PRIVATE_KEY"
-
-        context = {
-            'public_pem': instance.pubkey,
-            'test_magnet': test_magnet,
-            'signature_b64': signature_b64,
-        }
-        script_content = render_to_string('admin/test_key_script.txt', context)
-
-        response = HttpResponse(script_content, content_type='text/plain')
-        response['Content-Disposition'] = 'attachment; filename="test_keys.py"'
-        return response
-
-    generate_test_script.short_description = "Generate key test script"
+    generate_keys.short_description = "Generate and encrypt keys for selected instances"
 
 @admin.register(Alias)
 class AliasAdmin(admin.ModelAdmin):
