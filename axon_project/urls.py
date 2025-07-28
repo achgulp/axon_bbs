59348eef8f4ffa705da48414a13c79fc9ca18ffc
achgulp <achgulp@jamdata.net>
2025-07-27 20:29:33 -0400

    Update 20250727202933

--- axon_project/urls.py
+++ axon_project/urls.py
@@ -4,7 +4,6 @@ URL configuration for axon_project project.
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-# --- Import settings and static for media files ---
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -13,10 +12,10 @@ urlpatterns = [
     # Route for the Django admin interface
     path('admin/', admin.site.urls),
     
-    # Route for our application's API
+    # CORRECT ORDER: Route for our application's API must come BEFORE the catch-all
     path('api/', include('api.urls')),
     
-    # Add this: A catch-all route to serve the React app's index.html
+    # Catch-all route to serve the React app's index.html
     # for any route not handled by the above.
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
