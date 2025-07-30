4e7459e6d1d84457a561b88d3b6b14b312613b73
achgulp <achgulp@jamdata.net>
2025-07-30 11:56:27 -0400

    Update 20250730115627

--- axon_project/urls.py
+++ axon_project/urls.py
@@ -8,6 +8,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+# ✅ NEW: A custom view to force browsers not to cache the main app page.
+class NoCacheTemplateView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
+
 urlpatterns = [
     # Route for the Django admin interface
     path('admin/', admin.site.urls),
@@ -16,8 +25,8 @@ urlpatterns = [
     path('api/', include('api.urls')),
     
     # Catch-all route to serve the React app's index.html
-    # for any route not handled by the above.
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    # using our new no-cache view.
+    re_path(r'^.*', NoCacheTemplateView.as_view(template_name='index.html')),
 ]
 
 # This block to serve media files in development remains the same
