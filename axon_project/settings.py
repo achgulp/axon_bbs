8615817c4ba6130c76f7510bdead20bb24ad3669
achgulp <achgulp@jamdata.net>
2025-07-30 18:02:07 -0400

    Update 20250730180207

--- axon_project/settings.py
+++ axon_project/settings.py
@@ -88,6 +88,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# UPDATED: Point specifically to the 'static' sub-folder in the build directory.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 ]
