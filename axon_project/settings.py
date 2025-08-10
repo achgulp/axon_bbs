dd3c41d7aaf1190645f325886be06a675b5d00d9
achgulp <achgulp@jamdata.net>
2025-08-10 10:33:27 -0400

    Update 20250810103327

--- axon_project/settings.py
+++ axon_project/settings.py
@@ -105,7 +105,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://192.168.58.173:3000',
+    'http://0.0.0.0:3000',
     'http://*.onion',
 ]
 
