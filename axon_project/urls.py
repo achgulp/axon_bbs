1f87e3a3030f8804db8546c35ea6dfe19b99734d
achgulp <achgulp@jamdata.net>
2025-09-16 16:45:49 -0400

    Update 20250916164549

--- axon_project/urls.py
+++ axon_project/urls.py
@@ -8,11 +8,11 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 
 # Full path: axon_bbs/axon_project/urls.py
@@ -23,7 +23,6 @@ import os
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
-# UPDATED: Added the missing import for static file serving
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.views.static import serve
@@ -42,9 +41,16 @@ urlpatterns = [
     re_path(r'^(?P<path>manifest\.json)$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'frontend/build')}),
     re_path(r'^(?P<path>favicon\.ico)$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'frontend/build')}),
     re_path(r'^(?P<path>axon\.png)$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'frontend/build')}),
-    re_path(r'^.*', NoCacheTemplateView.as_view(template_name='index.html')),
 ]
 
-# This block allows the development server to serve user-uploaded media files (like avatars)
+# --- START FIX ---
+# This block is now placed BEFORE the final catch-all pattern. This ensures
+# that requests for media files are correctly served by Django's development server.
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# --- END FIX ---
+
+# This catch-all must be the LAST pattern.
+urlpatterns += [
+    re_path(r'^.*', NoCacheTemplateView.as_view(template_name='index.html')),
+]
