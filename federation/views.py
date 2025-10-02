415430583e7e3f3b307ff2488b9416b94cea1f95
achgulp <achgulp@jamdata.net>
2025-10-01 23:35:07 -0400

    Update GEMINI_20251001233507

--- federation/views.py
+++ federation/views.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-
+#
 
 # Full path: axon_bbs/federation/views.py
 from rest_framework import generics, permissions, status, views
